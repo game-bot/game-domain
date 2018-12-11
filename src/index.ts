9d18314f4115c7bc3bd9d4998fc87d3e8014f3d4
Mitica <canteadumitru@gmail.com>
2018-12-12 01:20:50 +0200

    module export

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+
+export { GameInfo, getAllGamesInfo, getGameInfo } from './game-info';
+export { GameJobInfo, getGameJobsInfo } from './game-job-info';
+export { JobBuilder } from './job-builder';
+export { Player } from './player';
